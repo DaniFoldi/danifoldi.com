67fbfc1fedd7e586301ac0c8b6a55b6c1b372d63
Dániel Földi <dani@stefan.hu>
2023-10-16 10:09:36 +0200

    :recycle: Some changes

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: cloudflare()
-})
+  adapter: cloudflare(),
+  integrations: [vue()]
+});
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content'
 
 
 export const collections = {
-  work: defineCollection({
+  blog: defineCollection({
     schema: z.object({
       title: z.string(),
       description: z.string(),
